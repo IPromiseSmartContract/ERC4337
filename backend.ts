06fa080a1daa2a32f4d1fa3e144e54b7dd536a94
parker178912 <parker178912.en08@nycu.edu.tw>
2023-06-06 11:29:45 +0800

    feat: add createPaymaster and fix else

--- backend.ts
+++ backend.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import { EntryPoint__factory, DepositPaymaster__factory, SimpleAccountFactory__factory } from "./typechain-types";
+import { EntryPoint__factory, DepositPaymaster__factory, SimpleAccountFactory__factory, PaymasterFactory__factory } from "./typechain-types";
 import { config as envConfig } from "dotenv";
 envConfig();
 /*
@@ -11,25 +11,43 @@ const provider = new ethers.providers.JsonRpcProvider(SEPOLIA_URL);
 const signer = new ethers.Wallet(SEPOLIA_PRIVATE_KEYS!, provider);
 const enrtryPointAddress = "0x9FA609A4430218a20aF170e246c1E35fbaCc3485";
 const entryPoint = EntryPoint__factory.connect(enrtryPointAddress, signer);
-const paymaterAddress = "0x8f9f13950Aa1d5445e11E7656C169ef22Eaa2811"
+const paymaterFactoryAddress = "0x5D0870C57B807278f54F624f1196014C6D1E0933"
+const paymasterFactory = PaymasterFactory__factory.connect(paymaterFactoryAddress, signer);
+const paymaterAddress = "0xb0555ef9f0D4D40A456E9949bf187f0d1793094f"
 const paymaster = DepositPaymaster__factory.connect(paymaterAddress, signer);
 const SimpleAccountFactoryAddress = "0xE334Ac1DD0e0f89c2B1e45a9a4cA42EC00b57067"
 const simpleAccountFactory = SimpleAccountFactory__factory.connect(SimpleAccountFactoryAddress, signer);
 
+async function createPaymaster(enrtryPointAddress: string) {
+    // Success !!
+    /*  Create a new Paymaster. 
+        call function :
+            paymasterFactory.createPaymaster
+    */
+    try {
+        const tx = await paymasterFactory.createPaymaster(enrtryPointAddress);
+        await tx.wait();
+        console.log(tx);
+        console.log("success");
+    } catch (error) {
+        console.error("error:", error);
+    }
+}
+
 async function addStake(unstakeDelaySec: number, stakeValue: string) {
-    // Fail !! Because the paymaster owner is not the creater but the factory.
+    // Success !!
     /*  For Paymaster owner stake ETH into entrypoint. 
         call function :  
             paymaster.addStake -> entrypoint.addStake
     */
     try {
-        const ownerAddress = await paymaster.getOwner()//.owner();
+        const ownerAddress = await paymaster.getOwner()
         console.log("owner Address:", ownerAddress, "signer Address", signer.address);
         const value = ethers.utils.parseEther(stakeValue);
         const tx = await paymaster.addStake(unstakeDelaySec, { value: value });
         await tx.wait();
-        //const result = await entryPoint.balanceOf(paymaster.address);
-        //console.log(result);
+        const result = await entryPoint.deposits(paymaster.address);
+        console.log(result);
         console.log("success");
     } catch (error) {
         console.error("error:", error);
@@ -37,7 +55,7 @@ async function addStake(unstakeDelaySec: number, stakeValue: string) {
 }
 
 async function depositTo(depositValue: string, paymasterAccount: string) {
-    // Fail !! Because the paymaster hasn't stake ETH.
+    // Success !!
     /*  Deposit ETH into entrypoint for fee and assign to a paymaster (which has already stake ETH). 
         call function :
             entrypoint.depositTo
@@ -106,7 +124,8 @@ async function addDepositFor(token: string, account: string, amount: number) {
     }
 }
 
-addStake(13, '0.01');
-// depositTo('0x7626BE17071b0656f4F52eA3D0A2696De6019a72', '0.01')
+// createPaymaster(enrtryPointAddress);
+// addStake(13, '0.01');
+// depositTo('0.01', paymaterAddress)
 // addToken('0x1C02053E9565DF6178eCaAD166D4cB9F8431107b', '0xEc6E432Cd61DAD1BAd43D32dE10e78Ac3785c790')
-// createAA('0xD10893cA2A290f02b05810beAE5027EBB43EaE94', 0)
+// createAA(signer.address, 0)
