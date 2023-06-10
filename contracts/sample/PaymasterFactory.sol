7b73ed0634690e7b306e2d95a5483ad491a502a6
alan890104 <alan890104@gmail.com>
2023-06-10 17:03:24 +0800

    feat: add indexed on owner

--- contracts/sample/PaymasterFactory.sol
+++ contracts/sample/PaymasterFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.15;
 import "./DepositPaymaster.sol";
 
 contract PaymasterFactory {
-    event PaymasterCreated(address paymaster, address owner);
+    event PaymasterCreated(address paymaster, address indexed owner);
 
     function createPaymaster(IEntryPoint entrypoint) public {
         DepositPaymaster paymaster = new DepositPaymaster(entrypoint, msg.sender);
